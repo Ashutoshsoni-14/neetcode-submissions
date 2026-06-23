00d4b31fd02a914a95a1d082e5dc55a10b82e88b
Ashutosh Soni <ashutoshhh.14@gmail.com>
2026-06-23 11:41:28 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if(st.size() == 0) return false;
+                if (c == ')' && st.top() == '(' || c == '}' && st.top() == '{' ||
+                    c == ']' && st.top() == '[') {
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.size() == 0;
+    }
+};
