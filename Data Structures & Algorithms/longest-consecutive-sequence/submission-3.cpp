a84105f27812fe1387189e6e510888d228ad17e2
Ashutosh Soni <ashutoshhh.14@gmail.com>
2026-07-14 16:49:20 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        if(nums.size() == 1) return 1;
+        sort(nums.begin(),nums.end());
+        int lcs = 0;
+        int currLCS = 1;
+        for(int i = 1;i<nums.size();i++){
+            if(nums[i] == nums[i-1]+1){
+                currLCS++;
+            }
+            else if(nums[i] == nums[i-1]){
+                currLCS = currLCS;
+            }
+            else{
+                currLCS = 1;
+            }
+            lcs = max(lcs , currLCS);
+        }
+        return lcs;
+    }
+};
