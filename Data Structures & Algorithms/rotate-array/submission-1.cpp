38ad0b3ebc6066252d366c66705c270cfa52bc07
Ashutosh Soni <ashutoshhh.14@gmail.com>
2026-07-19 00:11:43 +0530

    Add: rotate-array - submission-1

---
+++ Data Structures & Algorithms/rotate-array/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> v = nums;
+        int n = nums.size();
+        k = k % n;
+        for(int i = 0; i < n; i++){
+            nums[i] = v[(i-k+n)%n];
+        }
+    }
+};
