abea4489aba874ef06d54b11790c8f56ca9dd87f
Ashutosh Soni <ashutoshhh.14@gmail.com>
2026-08-11 00:37:04 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> current;
+        sort(nums.begin(),nums.end());
+        backtrack(0,nums,current,ans);
+        return ans;
+    }
+    void backtrack(int start , vector<int>& nums , vector<int> &current , vector<vector<int>> &ans){
+        ans.push_back(current);
+        for(int i = start; i < nums.size() ;i++){
+            if(i > start && nums[i] == nums[i-1]) continue;
+            current.push_back(nums[i]);
+            backtrack(i+1, nums, current, ans);
+            current.pop_back();
+        }
+    }
+};
