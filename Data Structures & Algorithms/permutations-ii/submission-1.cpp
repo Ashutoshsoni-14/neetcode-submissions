20cf655df82908178840ab8ef7adeba98093eaa0
Ashutosh Soni <ashutoshhh.14@gmail.com>
2026-08-12 13:06:59 +0530

    Add: permutations-ii - submission-1

---
+++ Data Structures & Algorithms/permutations-ii/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void backtrack(int index, vector<int> &nums){
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+        unordered_set<int> s;
+        for(int i = index; i<nums.size(); i++){
+            if(s.count(nums[i])){
+                continue;
+            }
+            s.insert(nums[i]);
+            swap(nums[i],nums[index]);
+            backtrack(index+1, nums);
+            swap(nums[i],nums[index]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        backtrack(0,nums);
+        return ans;
+    }
+};
